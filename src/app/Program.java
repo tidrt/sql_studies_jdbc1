74bd30a33cb05a0f17b1221d84a7b070be0fe3d8
Tiago <tiago@ufrgs.br>
2022-10-25 10:20:43 -0300

    first studie about sql with jdbc

---
+++ src/app/Program.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = DB.getConnection(); 
+		DB.closeConnection();
+	}
+
+}
